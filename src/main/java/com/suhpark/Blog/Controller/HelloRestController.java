e20a08eae62a9f2020e6b0c89391185571d9adbc
suhpark <laztwo@hanmail.net>
2018-06-19 22:59:12 +0900

    http://localhost:8080/swagger-ui.html

--- src/main/java/com/suhpark/Blog/Controller/HelloRestController.java
+++ src/main/java/com/suhpark/Blog/Controller/HelloRestController.java
@@ -5,6 +5,7 @@ import com.suhpark.Blog.dao.HelloDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class HelloRestController {
     @Autowired
     private HelloDao helloDao;
 
-    @RequestMapping("/add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Hello add(Hello hello) {
 
         Hello helloData = helloDao.save(hello);
@@ -22,7 +23,7 @@ public class HelloRestController {
         return helloData;
     }
 
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<Hello> list(Model model) {
 
         List<Hello> helloList = helloDao.findAll();
@@ -30,7 +31,7 @@ public class HelloRestController {
         return helloList;
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "hello, world!";
     }
